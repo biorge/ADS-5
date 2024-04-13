9d94f67a67a5a96993999a3894751f4a54919787
Anton <150729033+biorge@users.noreply.github.com>
2024-04-13 19:37:01 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,9 +2,42 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template<typename T, int size>
+template <typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+  T* stackArray;
+  int top;
+
+ public:
+  TStack() {
+    stackArray = new T[size];
+    top = -1;
+  }
+
+  ~TStack() { delete[] stackArray; }
+
+  void push(T value) {
+    if (top >= size - 1) {
+      throw "Stack overflow";
+    }
+    stackArray[++top] = value;
+  }
+
+  T pop() {
+    if (top < 0) {
+      throw "Stack is empty";
+    }
+    return stackArray[top--];
+  }
+
+  T get() {
+    if (top < 0) {
+      throw "Stack is empty";
+    }
+    return stackArray[top];
+  }
+
+  bool isEmpty() { return (top == -1); }
 };
 
 #endif  // INCLUDE_TSTACK_H_
