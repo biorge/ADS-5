afdc04e32e9e6af075175aeb19e257d25c3869bc
Anton <150729033+biorge@users.noreply.github.com>
2024-04-13 19:42:30 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,12 +3,88 @@
 #include <map>
 #include "tstack.h"
 
+bool isOperator(char c) {
+  return (c == '+' || c == '-' || c == '*' || c == '/');
+}
+
+int priority(char op) {
+  switch (op) {
+    case '+':
+    case '-':
+      return 1;
+    case '*':
+    case '/':
+      return 2;
+    default:
+      return 0;
+  }
+}
+
 std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+  std::string pstfx;
+TStack<char, 100> stack;
+
+for (char c : inf) {
+    if (c == ' ') continue; 
+    if (isdigit(c)) {
+        pstfx += c; 
+    }
+    else if (isOperator(c)) {
+        while (!stack.isEmpty() && priority(stack.get()) >= priority(c)) {
+            pstfx += " ";
+            pstfx += stack.pop(); 
+        }
+        pstfx += " ";
+        stack.push(c); 
+    }
+    else if (c == '(') {
+        stack.push(c); 
+    }
+    else if (c == ')') {
+        while (!stack.isEmpty() && stack.get() != '(') {
+            pstfx += " ";
+            pstfx += stack.pop(); 
+        }
+        stack.pop(); 
+    }
+}
+
+while (!stack.isEmpty()) {
+    pstfx += " ";
+    pstfx += stack.pop(); 
+}
+
+return pstfx;
 }
 
 int eval(std::string pref) {
-  // добавьте код
-  return 0;
+ TStack<int, 100> stack;
+int operand1, operand2;
+
+for (char c : post) {
+    if (c == ' ') continue; 
+    if (isdigit(c)) {
+        stack.push(c - '0'); 
+    }
+    else if (isOperator(c)) {
+        operand2 = stack.pop();
+        operand1 = stack.pop();
+        switch (c) {
+        case '+':
+            stack.push(operand1 + operand2);
+            break;
+        case '-':
+            stack.push(operand1 - operand2);
+            break;
+        case '*':
+            stack.push(operand1 * operand2);
+            break;
+        case '/':
+            stack.push(operand1 / operand2);
+            break;
+        }
+    }
+}
+
+return stack.pop();
 }
